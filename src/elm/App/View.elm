b8a61c770a2fded7cb52e6de06666cf4b9302d59
David Cumps <david@cumps.be>
2016-05-20 00:54:52 +0200

    Attempt to nest components

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -11,7 +11,9 @@ import Html.App as App exposing (map)
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ App.map Header (Header.view model.header)
-    , App.map Authentication (Authentication.view model.authentication)
-    ]
+  let
+    content =
+      [ App.map Header (Header.view model.header) ]
+  in
+    div []
+      [ App.map Authentication (Authentication.view model.authentication content) ]
--- src/elm/Authentication/View.elm
+++ src/elm/Authentication/View.elm
@@ -3,36 +3,29 @@ module Authentication.View exposing (view)
 import Authentication.Model exposing (Model)
 import Authentication.Msg exposing (Msg(..))
 
-import Html exposing (Html, div, button, text, img, ul, li)
-import Html.Attributes exposing (src, style)
+import Html exposing (Html, div, button, text, p)
 import Html.Events exposing (onClick)
 
 import Authentication.Auth0 as Auth0 exposing (AuthenticationState(LoggedIn), UserProfile)
 
-view : Model -> Html Msg
-view model =
+view : Model -> List (Html Msg) -> Html Msg
+view model content =
     case model.state of
       Auth0.LoggedIn userData ->
-        loggedInView Logout userData.profile
+        loggedInView Logout userData.profile content
 
       Auth0.LoggedOut ->
         loggedOutView ShowLogin
 
 
-loggedInView : Msg -> UserProfile -> Html Msg
-loggedInView logoutAction profile =
+loggedInView : Msg -> UserProfile -> List (Html Msg) -> Html Msg
+loggedInView logoutAction profile content =
   div []
-    [ div []
-        [ img [ src profile.picture ] []
-        , div [ style [ ("margin-left", "1em"), ("margin-right", "1em") ] ] [ text profile.name ]
-        , button
-            [ onClick logoutAction ]
-            [ text "Logout" ]
-        ]
-    , ul []
-        [ li [] [ text <| "Name: " ++ profile.name ]
-        , li [] [ text <| "Email: " ++ profile.email ]
-        ]
+    [ p [] [ text <| profile.name ++ " (" ++ profile.email ++ ")" ]
+    , button
+        [ onClick logoutAction ]
+        [ text "Logout" ]
+    , div [] content
     ]
 
 loggedOutView : Msg -> Html Msg
